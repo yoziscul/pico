40232292db0abd0edc96206a24621f4328de2bf1
Zeptar Hello <thezeptar@gmail.com>
2024-04-27 06:11:23 +0000

    refactor(cmds): /rps
    I realised the this command needed way more help than it had, so I added a accept/decline menu before it started, then restored back the await method instead of making a collector, which decreases the percentage of the unknown interaction error.

--- src/command/rps.js
+++ src/command/rps.js
@@ -32,84 +32,94 @@ const command = {
 
         const embed = new EmbedBuilder()
             .setTitle('Rock, Paper, Scissors!')
-            .setDescription`${user}, you have been challenged by ${interaction.user}.`
+            .setDescription(`${user}, you have been challenged by ${interaction.user}.`)
             .setColor('DarkGreen');
 
-        const row = new ActionRowBuilder()
+        const initialRow = new ActionRowBuilder()
             .addComponents(
-                choices.map((choice) => {
-                    return new ButtonBuilder()
-                        .setCustomId(choice.name)
-                        .setLabel(choice.name)
-                        .setStyle(2)
-                        .setEmoji(choice.emoji);
-                }),
+                new ButtonBuilder()
+                    .setCustomId('accept')
+                    .setLabel('Accept')
+                    .setStyle(3),
+                new ButtonBuilder()
+                    .setCustomId('decline')
+                    .setLabel('Decline')
+                    .setStyle(4),
             );
 
-        const reply = await interaction.editReply({
-            content: `\`${user.username}\`, pick one of the buttons below to start.`,
+        const initialReply = await interaction.editReply({
+            content: `\`${user.username}\`, pick one of the buttons below to accept or decline.`,
             embeds: [embed],
-            components: [row],
+            components: [initialRow],
         });
 
-        const userCollector = reply.createMessageComponentCollector({ time: 30000 });
-        
-        userCollector.on('collect', async (i) => {
-            if (i.user.id !== user.id) {
-                return await i.reply({ embeds: embedErr`It's not your turn right now.`, ephemeral: true });
-            }
+        const filter = (i) => i.user.id === user.id;
+        const iFilter = (i) => i.user.id === interaction.user.id;
 
-            await i.deferUpdate();
-            const userChoice = choices.find((choice) => choice.name === i.customId);
+        const i = await initialReply.awaitMessageComponent({ filter, time: 30000 })
+            .catch(async () => {
+                await interaction.editReply({
+                    content: 'Game over.',
+                    embeds: [embedErr("The following user didn't accept in time.")]
+                });
+            });
 
-            embed.setDescription`It's currently ${interaction.user}'s turn.`;
+        await i.deferUpdate();
 
-            await i.editReply({
-                content: `\`${interaction.user.username}\`, it's your turn to choose.`,
+        if (i.customId === 'accept') {
+            const row = new ActionRowBuilder()
+                .addComponents(
+                    choices.map((choice) => {
+                        return new ButtonBuilder()
+                            .setCustomId(choice.name)
+                            .setLabel(choice.name)
+                            .setStyle(2)
+                            .setEmoji(choice.emoji);
+                    }),
+                );
+
+            const reply = await i.editReply({
+                content: `${user}, it's your turn to choose.`,
+                embeds: [embed],
+                components: [row],
+            });
+
+            const j = await reply.awaitMessageComponent({ filter, time: 30000 });
+            const userChoice = choices.find((choice) => choice.name === j.customId);
+            await j.deferUpdate();
+
+            embed.setDescription(`It's currently ${interaction.user}'s turn.`);
+
+            await j.editReply({
+                content: `${interaction.user}, it's your turn to choose.`,
                 embeds: [embed],
             });
 
-            const authorCollector = reply.createMessageComponentCollector({ time: 30000 });
+            const k = await reply.awaitMessageComponent({ filter: iFilter, time: 30000 });
+            const authorChoice = choices.find((choice) => choice.name === k.customId);
+            await k.deferUpdate();
 
-            authorCollector.on('collect', async (j) => {
-                if (j.user.id !== interaction.user.id) {
-                    return await j.reply({ embeds: embedErr`It's not your turn right now.`, ephemeral: true });
-                }
+            let result = 'Nobody won, it was a tie!';
 
-                await j.deferUpdate();
-                const authorChoice = choices.find((choice) => choice.name === j.customId);
-
-                let result = 'Nobody won, it was a tie!';
-
-                if (userChoice.beats === authorChoice.name || authorChoice.beats === userChoice.name) {
-                    result = (userChoice.beats === authorChoice.name) ? `${user} is the winner!` : `${interaction.user} is the winner!`;
-                }
-
-                const userPicked = `${user} picked ${userChoice.name} ${userChoice.emoji}`;
-                const authorPicked = `${interaction.user} picked ${authorChoice.name} ${authorChoice.emoji}`;
-
-                embed.setDescription`${userPicked}\n${authorPicked}`;
-                await j.editReply({ content: result, embeds: [embed] });
-            });
-
-            authorCollector.on('end', () => {
-                if (!authorCollector.collected.size) {
-                    reply.edit({
-                        content: 'Game ended.',
-                        embeds: [embedErr`${user} didn't respond in time.`]
-                    });
-                }
-            });
-        });
-
-        userCollector.on('end', () => {
-            if (!userCollector.collected.size) {
-                reply.edit({
-                    content: 'Game ended.',
-                    embeds: [embedErr`${user} didn't respond in time.`]
-                });
+            if (userChoice.beats === authorChoice.name || authorChoice.beats === userChoice.name) {
+                result = (userChoice.beats === authorChoice.name) ? `${user} is the winner!` : `${interaction.user} is the winner!`;
             }
-        });
+
+            const userPicked = `${user} picked ${userChoice.name} ${userChoice.emoji}`;
+            const authorPicked = `${interaction.user} picked ${authorChoice.name} ${authorChoice.emoji}`;
+
+            embed.setDescription(`${userPicked}\n${authorPicked}`);
+
+            await k.editReply({
+                content: result,
+                embeds: [embed],
+            });
+        } else {
+            return await i.editReply({
+                content: 'Game over.',
+                embeds: [embedErr('The following user has declined the request.')],
+            });
+        }
     }
 };
 
